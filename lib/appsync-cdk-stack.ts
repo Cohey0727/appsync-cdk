7908a3d0dd4526614f9aaeb9f7832f4a8008d7ea
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-02-03 00:13:33 +0900

    master

--- lib/appsync-cdk-stack.ts
+++ lib/appsync-cdk-stack.ts
@@ -1,5 +1,7 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as appsync from "aws-cdk";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AppsyncCdkStack extends Stack {
