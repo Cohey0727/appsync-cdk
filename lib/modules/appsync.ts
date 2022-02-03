8c57ed633b4d0836c1486c0658a9822dbb431b73
Kohei <ohayousagi.ac.kook0727@gmail.com>
2022-02-04 08:02:26 +0900

    master

--- lib/modules/appsync.ts
+++ lib/modules/appsync.ts
@@ -1,25 +1,9 @@
-import { Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
+import { Stack } from "aws-cdk-lib";
 import * as appsync from "aws-cdk-lib/aws-appsync";
 import { createStackName } from "../modules";
 import schemas from "../../app/schemas";
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export default class MainStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-    createGrapqlApi(this);
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppsyncCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
-
-function createGrapqlApi(scope: Stack) {
+export function createGrapqlApi(scope: Stack) {
   const name = createStackName("GraphQLApi");
   const cfnGraphQLApi = new appsync.CfnGraphQLApi(scope, "CfnGraphQLApi", {
     authenticationType: "API_KEY",
